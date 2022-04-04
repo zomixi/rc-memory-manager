9d15978ad6acbf1e81b6cdc6b78aa6755e648fc4
zomixi <zomixi@qq.com>
2022-04-04 23:17:00 +0800

    chore: remove babel-plugin-import

--- .fatherrc.ts
+++ .fatherrc.ts
@@ -6,14 +6,4 @@ export default {
     eslint: true,
     prettier: true,
   },
-  extraBabelPlugins: [
-    [
-      'babel-plugin-import',
-      {
-        libraryName: 'antd',
-        libraryDirectory: 'es',
-        style: true,
-      },
-    ],
-  ],
 };
